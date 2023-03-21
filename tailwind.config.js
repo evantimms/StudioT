1bd10a4d8b9842038283ab5f5294a6456a2da94f
Evan Timms <evantimms@MacBook-Air-2.local>
2023-03-21 14:34:28 -0400

    Added first section

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            'default': 'Helvetica, Arial, sans-serif'
+        },
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+        }
+    },
+    plugins: [],
+  }
